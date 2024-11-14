37b81fdea4e2ca6e974704c227159102df46ca90
Nesss86 <Nessymonster86@gmail.com>
2024-11-14 10:05:36 -0500

    Fixed typos and added function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,15 @@
-const eqArrays = function(array1, array2){
+const assertArraysEqual = function (array1, array2){
   if (array1.length !== array2.length) {
-    return false;
+    console.log("These Arrays Are Not Equal in Length");
   }
   for (let i = 0; i <array1.length; i++) {
     if (array1[i] !== array2[i]) {
-      return false;
+      console.log("One or More Elements Don't Match");
+      return;
     }
   }
-  return true;
+  console.log("Both Arrays Match!");
 };
-
-
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
@@ -19,8 +18,8 @@ const assertEqual = function(actual, expected) {
     console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
 // TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+assertArraysEqual([1, 2, 3], [1, 2, 3]); 
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
+assertArraysEqual([1, 2, 3], [1, 2]); 
