8836d3be95f72e1c9ebc7bbe5203cf28df694c46
Nesss86 <Nessymonster86@gmail.com>
2024-11-19 19:49:56 -0500

    Refactored tail.js and added test file

--- assertEqual.js
+++ assertEqual.js
@@ -1,4 +1,4 @@
-// FUNCTION IMPLEMENTATION
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
@@ -7,6 +7,6 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-module.exports = assertEqual
+module.exports = assertEqual;
 
 
