c6b69ccf6a6190e92594350f1b8c18bed065abb9
Nesss86 <Nessymonster86@gmail.com>
2024-11-19 20:34:02 -0500

    Refactored assertArraysEqual.js and added a test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,6 +1,7 @@
 const assertArraysEqual = function (array1, array2){
   if (array1.length !== array2.length) {
     console.log("These Arrays Are Not Equal in Length");
+    return;
   }
   for (let i = 0; i <array1.length; i++) {
     if (array1[i] !== array2[i]) {
@@ -19,7 +20,4 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [1, 2, 4]);
-assertArraysEqual([1, 2, 3], [1, 2]); 
+module.exports = assertArraysEqual;
