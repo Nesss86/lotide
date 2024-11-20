40bb1ace804f0d02d0599023a5b497da71f1257f
Nesss86 <Nessymonster86@gmail.com>
2024-11-19 22:34:25 -0500

    Added in test case for middleTest.js

--- test/middleTest.js
+++ test/middleTest.js
@@ -25,4 +25,9 @@ describe("#middle", () => {
   it("returns [3, 4] for [1, 2, 3, 4, 5, 6]", () => {
     assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
   });
+  it("should not alter the original array length", () => {
+    const words = [1, 2, 3, 4, 5];
+    middle(words);
+    assert.strictEqual(words.length, 5);
+  });
 });
